57ae36bbe13b50721ba9e65d01d9da40caf0b4f0
satriaenur <satrianur.sienur@gmail.com>
2015-10-17 19:23:06 +0700

    Initial commit

---
+++ app/src/main/java/com/example/rizki/belajarmaps/GPSTracker.java
@@ -0,0 +1,146 @@
+package com.example.rizki.belajarmaps;
+
+import android.app.AlertDialog;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.content.*;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by rizki on 10/17/15.
+ */
+public class GPSTracker extends Service implements LocationListener {
+
+    private final Context context;
+
+    boolean isGPSEnabled = false;
+
+    boolean isNetworkEnabled = false;
+
+    boolean canGetLocation = false;
+
+    Location location;
+    double lat;
+    double longi;
+
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
+
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1;
+
+    protected LocationManager locationManager;
+
+
+    public GPSTracker(Context context) {
+        this.context = context;
+        getLocation();
+    }
+
+    public Location getLocation(){
+        try{
+            locationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
+
+            isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if(isGPSEnabled && isNetworkEnabled){
+                this.canGetLocation = true;
+                if(isGPSEnabled){
+                    if(location == null){
+                        locationManager.requestLocationUpdates(
+                                LocationManager.GPS_PROVIDER,
+                                MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                        if(locationManager!=null){
+                            location = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+                            if(location!=null){
+                                lat = location.getLatitude();
+                                longi = location.getLongitude();
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return location;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    public double getLat(){
+        if(location!=null){
+            lat = location.getLatitude();
+        }
+        return lat;
+    }
+
+    public double getLongi(){
+        if(location!=null){
+            longi = location.getLongitude();
+        }
+        return longi;
+    }
+
+    public boolean canGetLocation(){
+        return this.canGetLocation;
+    }
+
+    public void showSettingAlert(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(context);
+
+        alertDialog.setTitle("GPS Enabled");
+
+        alertDialog.setMessage("GPS is not Enabled, Do you want to turn it on?");
+
+        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                context.startActivity(intent);
+            }
+        });
+
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        alertDialog.show();
+    }
+}
---
+++ app/src/main/java/com/example/rizki/belajarmaps/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.rizki.belajarmaps;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment map = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.fragment);
+        map.getMapAsync(this);
+
+        GoogleMap nmap = map.getMap();
+        nmap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener(){
+
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                marker.showInfoWindow();
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map){
+        GPSTracker gps = new GPSTracker(this);
+        map.setMyLocationEnabled(true);
+        if(!gps.canGetLocation()){
+            gps.showSettingAlert();
+        }else {
+            Log.e("test", gps.getLat() + "");
+            Log.e("test", gps.getLongi() + "");
+            LatLng mapCenter = new LatLng(gps.getLat(), gps.getLongi());
+            LatLng kedua = new LatLng(-6.9757899,107.6328453);
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(mapCenter, 17));
+
+            map.addMarker(new MarkerOptions()
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.location_pin))
+                    .position(mapCenter)
+                    .flat(true)
+                    .rotation(245));
+            map.addMarker(new MarkerOptions()
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.location_pin))
+                    .position(kedua)
+                    .flat(true)
+                    .rotation(245));
+            CameraPosition cameraPosition = CameraPosition.builder()
+                    .target(mapCenter)
+                    .zoom(17)
+                    .bearing(90)
+                    .build();
+
+            map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition),2000,null);
+
+        }
+    }
+}
